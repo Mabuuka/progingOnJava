ec7f78d945f0c18be6f355bc69403bcc65bffd5a
Mabuuka <144621966+Mabuuka@users.noreply.github.com>
2023-09-14 00:38:43 +0300

    Create TestAuthor.java

---
+++ pr3/1/TestAuthor.java
@@ -0,0 +1,6 @@
+public class TestAuthor {
+    public static void main(String[] args) {
+        Author a = new Author("Сергей", "sergeySergeev@milo.com", 'm');
+        System.out.println(a);
+    }
+}
