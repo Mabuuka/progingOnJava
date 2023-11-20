774291a6b04381158f4a8e898640e58e66d5b3da
Mabuuka <144621966+Mabuuka@users.noreply.github.com>
2023-11-20 22:10:07 +0300

    Add files via upload

---
+++ pr 14/BoundedWaitList.java
@@ -0,0 +1,23 @@
+class BoundedWaitList<E> extends WaitList<E> {
+    private int capacity;
+
+    public BoundedWaitList(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    @Override
+    public void add(E element) {
+        if (content.size() < capacity) {
+            super.add(element);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", capacity=" + capacity;
+    }
+}
---
+++ pr 14/IWaitlist.java
@@ -0,0 +1,10 @@
+import java.util.Collection;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+interface IWaitList<E> {
+    void add(E element);
+    E remove();
+    boolean contains(E element);
+    boolean containsAll(Collection<E> c);
+    boolean isEmpty();
+}
