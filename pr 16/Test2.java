4e656d85c1eb85916945cf94d02d38cf91746c58
Mabuuka <144621966+Mabuuka@users.noreply.github.com>
2023-11-20 22:47:38 +0300

    Add files via upload

---
+++ pr 16/Test2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Test2 {
+    public static void main(String[] args) {
+        ThrowsDemo2 demo = new ThrowsDemo2();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a key:");
+        String key = scanner.nextLine();
+
+        try {
+            demo.printDetails(key);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ pr 16/ThrowsDemo2.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class ThrowsDemo2 { //тест3
+    public void getKey() {
+        Scanner myScanner = new Scanner(System.in);
+        String key = "";
+        while(key.equals("")) {
+            System.out.println("Enter a key:");
+            key = myScanner.nextLine();
+            try {
+                printDetails(key);
+            } catch (Exception e) {
+                System.out.println("Caught an exception in printDetails method.");
+                key = "";  // Reset key to give the user another attempt
+            }
+        }
+    }
+
+    public void printDetails(String key) throws Exception {
+        String message = getDetails(key);
+        System.out.println(message);
+    }
+
+    private String getDetails(String key) throws Exception {
+        if(key.equals("")) {
+            throw new Exception("Key set to empty string");
+        }
+        return "data for " + key;
+    }
+}
