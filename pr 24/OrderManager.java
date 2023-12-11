86e3f0b8810832604e5f28db1e0f8b0d33799239
Mabuuka <144621966+Mabuuka@users.noreply.github.com>
2023-12-12 02:09:52 +0300

    Update OrderManager.java

--- pr 24/OrderManager.java
+++ pr 24/OrderManager.java
@@ -1,39 +1,49 @@
 import java.util.HashMap;
 import java.util.Map;
 
-
-public class OrderManager { //2
-
-
+// Класс, управляющий заказами
+public class OrderManager {
+    // Хранение заказов в виде карты, где ключ - адрес, значение - заказ
     private Map<String, Order> orders;
 
+    // Конструктор класса
     public OrderManager() {
         orders = new HashMap<>();
     }
 
+    // Метод для добавления заказа по адресу
     public void addOrder(String address, Order order) {
         orders.put(address, order);
     }
 
+    // Метод для получения заказа по адресу
     public Order getOrder(String address) {
         return orders.get(address);
     }
 
+    // Метод для удаления заказа по адресу
     public void removeOrder(String address) {
         orders.remove(address);
     }
 
+    // Метод для добавления товара к заказу по адресу
     public void addItemToOrder(String address, Item item) {
         Order order = orders.get(address);
         if (order != null) {
-            order.addItem(item);
+            try {
+                order.addItem(item);
+            } catch (OrderAlreadyAddedException e) {
+                System.out.println("Item is already added to the order.");
+            }
         }
     }
 
+    // Метод для получения массива интернет-заказов
     public Order[] getInternetOrders() {
         return orders.values().toArray(new Order[0]);
     }
 
+    // Метод для получения общей стоимости интернет-заказов
     public double getTotalPriceOfInternetOrders() {
         double totalPrice = 0;
         for (Order order : orders.values()) {
@@ -42,6 +52,7 @@ public class OrderManager { //2
         return totalPrice;
     }
 
+    // Метод для получения общего количества товаров с определенным названием в интернет-заказах
     public int getTotalItemCountOfDish(String dishName) {
         int totalCount = 0;
         for (Order order : orders.values()) {
