35535601fcfa761c9612b7ff927dcd1ac505fb7a
Mabuuka <144621966+Mabuuka@users.noreply.github.com>
2023-12-12 02:09:31 +0300

    Update OrderAlreadyAddedException.java

--- pr 24/OrderAlreadyAddedException.java
+++ pr 24/OrderAlreadyAddedException.java
@@ -1,4 +1,6 @@
+// Исключение, представляющее ситуацию, когда заказ уже был добавлен
 public class OrderAlreadyAddedException extends RuntimeException {
+    // Конструктор, принимающий сообщение об ошибке
     public OrderAlreadyAddedException(String message) {
         super(message);
     }
