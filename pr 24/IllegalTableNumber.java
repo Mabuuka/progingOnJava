ca3fe197bb73c45d42d2ce4a3c8afc66b8e8c863
Mabuuka <144621966+Mabuuka@users.noreply.github.com>
2023-12-12 02:08:48 +0300

    Update IllegalTableNumber.java

--- pr 24/IllegalTableNumber.java
+++ pr 24/IllegalTableNumber.java
@@ -1,4 +1,6 @@
-public class IllegalTableNumber extends IllegalArgumentException {
+// Исключение, представляющее некорректный номер стола
+public class IllegalTableNumber extends RuntimeException {
+    // Конструктор, принимающий сообщение об ошибке
     public IllegalTableNumber(String message) {
         super(message);
     }
